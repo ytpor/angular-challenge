9da77431fd7e6933292782c98c61e0ccb103c558
YT Por <agtong+ytpor@gmail.com>
2025-10-16 10:21:33 +0800

    Clean up after integrating keycloak.

--- src/app/pages/blank.routes.ts
+++ src/app/pages/blank.routes.ts
@@ -1,17 +1,4 @@
 import { Routes } from '@angular/router';
 
-import { LoginComponent } from './login/login.component';
-import { loginGuard } from '../guards/login/login.guard';
-
 export const BlankRoutes: Routes = [
-  {
-    path: '',
-    children: [
-      {
-        path: 'login',
-        canActivate: [loginGuard],
-        component: LoginComponent,
-      },
-    ],
-  },
 ];
